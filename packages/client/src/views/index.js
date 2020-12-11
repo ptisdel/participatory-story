5fdd3ddea8a4c8151e2e303bd9921bbacf9a2f03
Patrick Tisdel <patricktisdel@outlook.com>
2020-12-10 21:13:29 -0500

    (refactor): change view folder names

--- packages/client/src/views/index.js
+++ packages/client/src/views/index.js
@@ -1,6 +1,6 @@
-export { CreateStoryView } from './create-story';
-export { HomeView } from './home';
-export { LoginView } from './login';
-export { PageNotFoundView } from './page-not-found';
-export { RegisterView } from './register';
-export { StoryView } from './story';
+export { CreateStoryView } from './create-story-view';
+export { HomeView } from './home-view';
+export { LoginView } from './login-view';
+export { PageNotFoundView } from './page-not-found-view';
+export { RegisterView } from './register-view';
+export { StoryView } from './story-view';
